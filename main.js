3a9a136edd0597d5a071d9bb99c98159449f7597
lyamtorres <lyamtorres@hotmail.com>
2021-10-29 12:29:52 +0200

    Add quit function

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-const {Builder, By, Key, util} = require("selenium-webdriver");
-async function example() {
+const {Builder, By, Key} = require("selenium-webdriver");
+const assert = require("assert");
+
+async function start() {
     let driver = await new Builder().forBrowser("chrome").build();
-    await driver.get("http://bing.com");
-    await driver.findElement(By.name("q")).sendKeys("Selenium", Key.RETURN);
+
+    await driver.get("https://bing.com");
+    await driver.findElement(By.id("sb_form_q")).sendKeys("Selenium", Key.RETURN);
+
+    await driver.quit();
 }
-example();
+
+start();
